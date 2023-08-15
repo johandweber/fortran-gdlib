ab9ff00ebbbaa45395e178f637771c29cd21e2ac
johandweber <johaweb@hotmail.com>
2023-08-16 00:54:14 +0200

    Update test_pipes.f90

--- tests/test_pipes.f90
+++ tests/test_pipes.f90
@@ -1,32 +1,28 @@
-program test_im_int_array
+program test_pipes
   use, intrinsic:: iso_c_binding
-  use, intrinsic:: iso_fortran_env
   use fortran_libgd
   implicit none
 
   integer(c_int), parameter:: w = 320_c_int
   integer(c_int), parameter:: h = 240_c_int
 
-
-  integer(c_int):: return_code
+  integer(c_int)    :: return_code
   integer(c_size_t) :: count
 
-  type(c_ptr) :: im, inpipe, outpipe, pngout
+  type(c_ptr) :: im, inpipe, outpipe
 
   integer(c_int8_t) :: pipematrix(1:3, 0:w-1, 0:h-1)
   integer(c_int8_t) :: pipematrix_linear(3*w*h)
   integer(c_int)    :: workmatrix(1:3, 0:w-1, 0:h-1)
-  integer(c_int)    :: workmatrix_linear(3*w*h)
 
-  integer(c_int)   :: yellow, greyvalue, x_counter, y_counter
+  integer(c_int)   :: x_counter, y_counter
+  integer(c_int)   :: yellow
   integer(c_int)   :: r, g, b
   
   integer:: frame_counter = 1
 
   integer(c_int), dimension(0:7) ::imagerect
-
   imagerect=[0,0,319,0,319,239,0,239]
-
 
   im = gdImageCreateTrueColor(w, h)
 
@@ -34,10 +30,17 @@ program test_im_int_array
 
   inpipe = gd_popen("ffmpeg -i inpics/swans.mp4 -f image2pipe"//&
        " -vcodec rawvideo -pix_fmt rgb24 -"//c_null_char, "r"//c_null_char)
+
+  if ( .not. c_associated(inpipe) ) &
+       stop 'Could not open input pipe. Is ffmpeg in your executable path?'
   
   outpipe = gd_popen("ffmpeg -y -f rawvideo"//&
        " -vcodec rawvideo -pix_fmt rgb24 -s 320x240 -r 25 -i -"//&
-  " -f mp4 -q:v 5 -an -vcodec mpeg4 outpics/purplewater.mp4"//c_null_char, "w"//c_null_char)
+       " -f mp4 -q:v 5 -an -vcodec mpeg4 outpics/purplewater.mp4"//c_null_char, "w"//c_null_char)
+
+  if ( .not. c_associated(outpipe) ) &
+      stop 'Could not open outpup pipe. Is ffmpeg in your executable path?'
+       
 
   print*, "start loop"
   do while(.true.)
@@ -50,7 +53,7 @@ program test_im_int_array
      
      do y_counter=0,gdImageSy(im)-1
         do x_counter=0,gdImageSx(im)-1
-           greyvalue = sum (workmatrix(:, x_counter, y_counter))/3
+           
            r =  workmatrix(1, x_counter, y_counter)
            g =  workmatrix(2, x_counter, y_counter)
            b =  workmatrix(3, x_counter, y_counter)
@@ -58,7 +61,6 @@ program test_im_int_array
            workmatrix(1, x_counter, y_counter) =  b
            workmatrix(2, x_counter, y_counter) =  r
            workmatrix(3, x_counter, y_counter) =  g
-           
               
         end do
      end do
@@ -66,12 +68,15 @@ program test_im_int_array
      call gdWriteImFromIntArray(im, workmatrix, w, h)
 
      call gdImageStringFT(im, imagerect, yellow,"/usr/share/fonts/opentype/freefont/FreeSans.otf"//c_null_char , &
-       16._c_double, 0._c_double ,10,20,"Fortran TV"//c_null_char)
+          16._c_double, 0._c_double ,10,20,"Fortran TV"//c_null_char)
+     call gdImageSetThickness(im, 3)
+     call gdImageLine(im, 0, 120, 319, 120, yellow)
+     call gdimageLine(im, 160, 0, 160, 239, yellow)
      
      call gdReadIntArrayFromIm(im, workmatrix, w, h)
      
      write(*,*) 'Workmatrix(1,0,0) :', workmatrix(1,0,0)
-     call gdIntArrayToUInt8Array(workmatrix,pipematrix,w,h)
+     call gdIntArrayToUInt8Array(workmatrix,pipematrix,w,h) 
 
      count= gd_fwrite_rawdata_matrix(pipematrix,w,h, outpipe)
      write(*,*) "Count = ",count
@@ -88,4 +93,4 @@ program test_im_int_array
   return_code = gd_fflush(outpipe)
   return_code = gd_pclose(outpipe)
 
-end program test_im_int_array
+end program test_pipes
